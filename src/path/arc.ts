f59bb15eef0630ac274e18dcfcc178a05b8a2e2d
jfalxa <jfalxa.sta@gmail.com>
2025-04-29 18:38:49 +0200

    adaptive arc sampling

--- src/path/arc.ts
+++ src/path/arc.ts
@@ -1,25 +1,17 @@
 import { Point, Vec2 } from "../math/vec2";
 import { BoundingBox } from "../utils/bounding-box";
+import { pointToLineDistance } from "../utils/polyline";
 import { Segment } from "./segment";
 
 export function arc(
   x: number,
   y: number,
-  startAngle: number,
-  endAngle: number,
   radius: number,
-  counterclockwise?: boolean,
-  segments = 10
+  startAngle = 0,
+  endAngle = 2 * Math.PI,
+  counterclockwise?: boolean
 ) {
-  return new Arc(
-    x,
-    y,
-    startAngle,
-    endAngle,
-    radius,
-    counterclockwise,
-    segments
-  );
+  return new Arc(x, y, radius, startAngle, endAngle, counterclockwise);
 }
 
 export class Arc extends Segment {
@@ -28,13 +20,12 @@ export class Arc extends Segment {
   constructor(
     x: number,
     y: number,
-    public startAngle: number,
-    public endAngle: number,
     public radius: number,
-    public counterclockwise?: boolean,
-    segments = 10
+    public startAngle = 0,
+    public endAngle = 2 * Math.PI,
+    public counterclockwise?: boolean
   ) {
-    super(x, y, segments);
+    super(x, y);
   }
 
   apply(path: Path2D, _control: Vec2, sx: number, sy: number) {
@@ -51,24 +42,24 @@ export class Arc extends Segment {
     );
   }
 
-  sample(_from: any, _control: any, sx: number, sy: number): Vec2[] {
+  sample(
+    _from: any,
+    _control: any,
+    sx: number,
+    sy: number,
+    tolerance: number
+  ): Vec2[] {
     const to = Arc.#P.copy(this.to).scale(sx, sy);
     const radius = this.radius * Math.min(sx, sy);
 
-    this.points.length = this.tolerance + 1;
-
-    for (let i = 0; i <= this.tolerance; i++) {
-      this.points[i] = Arc.sample(
-        to,
-        radius,
-        this.startAngle,
-        this.endAngle,
-        i / this.tolerance,
-        this.points[i]
-      );
-    }
-
-    return this.points;
+    return Arc.adaptiveSample(
+      to,
+      radius,
+      this.startAngle,
+      this.endAngle,
+      tolerance,
+      this.points
+    );
   }
 
   join(aabb: BoundingBox, _from: Vec2, _control: Vec2 | undefined) {
@@ -90,6 +81,48 @@ export class Arc extends Segment {
     const angle = startAngle + angleDiff * t;
     out.x = center.x + radius * Math.cos(angle);
     out.y = center.y + radius * Math.sin(angle);
+    return out;
+  }
+
+  static adaptiveSample(
+    center: Point,
+    radius: number,
+    startAngle: number,
+    endAngle: number,
+    tolerance: number,
+    out: Vec2[] = []
+  ) {
+    out[0] = new Vec2(
+      center.x + radius * Math.cos(startAngle),
+      center.y + radius * Math.sin(startAngle)
+    );
+
+    let i = 1;
+    const stack = [{ a: startAngle, b: endAngle }];
+
+    while (stack.length > 0) {
+      const { a, b } = stack.pop()!;
+
+      const p0 = Arc.sample(center, radius, a, b, 0);
+      const p1 = Arc.sample(center, radius, a, b, 1);
+      const pm = Arc.sample(center, radius, a, b, 0.5);
+
+      const err = pointToLineDistance(pm, p0, p1);
+
+      if (err <= tolerance) {
+        if (!out[i]) out[i] = new Vec2(0, 0);
+        out[i++].put(p1.x, p1.y);
+      } else {
+        let diff = b - a;
+        if (diff < 0) diff += Math.PI * 2;
+        const midAngle = a + diff * 0.5;
+
+        stack.push({ a: midAngle, b: b });
+        stack.push({ a: a, b: midAngle });
+      }
+    }
+
+    out.length = i;
     return out;
   }
 }
