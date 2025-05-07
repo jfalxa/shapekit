d6dd0c222dea9887d02e821bba92d0be8c213578
jfalxa <jfalxa.sta@gmail.com>
2025-05-08 01:41:18 +0200

    fix transformer

--- src/utils/transformer.ts
+++ src/utils/transformer.ts
@@ -1,16 +1,13 @@
 import { Matrix3 } from "../math/mat3";
-import { Vec2 } from "../math/vec2";
+import { v, Vec2 } from "../math/vec2";
 import { BoundingBox } from "./bounding-box";
 import { Renderable } from "../renderables/renderable";
 
 interface Snapshot {
-  renderable: Renderable;
   width: number;
   height: number;
-  localTransform: Matrix3;
-  parentTransform: Matrix3;
+  transform: Matrix3;
   invParentTransform: Matrix3;
-  screenTransform: Matrix3;
 }
 
 export class Transformer {
@@ -22,50 +19,46 @@ export class Transformer {
   skewY = 0;
   rotation = 0;
 
-  baseWidth = 0;
-  baseHeight = 0;
-
-  transform = new Matrix3();
-  invTransform = new Matrix3();
-
-  center = new Vec2(0, 0);
   obb = new BoundingBox();
 
-  snapshots = new Map<Renderable, Snapshot>();
+  #width = 0;
+  #height = 0;
 
+  #center = new Vec2(0, 0);
+  #pivot = new Vec2(0, 0);
+  #snapshots = new Map<Renderable, Snapshot>();
   #transform = new Matrix3();
-  #obb = new BoundingBox();
+  #invTransform = new Matrix3();
+  #selectionTransform = new Matrix3();
+  #selectionOBB = new BoundingBox();
 
   constructor(public selection: Renderable[]) {
-    this.update();
-  }
+    this.fit();
 
-  update() {
-    this.x = 0;
-    this.y = 0;
-    this.skewX = 0;
-    this.skewY = 0;
-    this.rotation = 0;
-
-    this.snapshots.clear();
-
-    if (this.selection.length === 1) {
-      this.snapshot(this.selection[0]);
-      this.obb.copy(this.selection[0].obb);
-      return;
-    }
-
-    this.obb.min.put(+Infinity);
-    this.obb.max.put(-Infinity);
+    this.width = this.#width = this.obb.width;
+    this.height = this.#height = this.obb.height;
+    this.#center.copy(this.obb.center);
 
     for (const renderable of this.selection) {
       this.snapshot(renderable);
-      this.obb.merge(renderable.obb);
     }
+  }
 
-    this.center.copy(this.obb.center);
-    this.width = this.baseWidth = this.obb.width;
-    this.height = this.baseHeight = this.obb.height;
+  commit(pivot?: Vec2) {
+    this.apply(pivot);
+    this.x = this.obb.center.x - this.#center.x;
+    this.y = this.obb.center.y - this.#center.y;
+  }
+
+  revert() {
+    this.x = 0;
+    this.y = 0;
+    this.width = this.#width;
+    this.height = this.#height;
+    this.rotation = 0;
+    this.skewX = 0;
+    this.skewY = 0;
+    this.apply();
   }
 
   snapshot(renderable: Renderable) {
@@ -75,58 +68,63 @@ export class Transformer {
     const localTransform = new Matrix3().compose(renderable);
     const parentTransform = new Matrix3(renderable.parent?.transform);
     const invParentTransform = new Matrix3(parentTransform).invert();
-    const screenTransform = new Matrix3(localTransform).transform(parentTransform); // prettier-ignore
+    const transform = new Matrix3(localTransform).transform(parentTransform);
 
-    this.snapshots.set(renderable, {
-      renderable,
+    this.#snapshots.set(renderable, {
       width,
       height,
-      localTransform,
-      parentTransform,
+      transform,
       invParentTransform,
-      screenTransform,
     });
   }
 
-  apply(from = this.center) {
-    const sx = this.baseWidth !== 0 ? this.width / this.baseWidth : 1;
-    const sy = this.baseHeight !== 0 ? this.height / this.baseHeight : 1;
-
-    this.transform
-      .identity()
-      .translate(-from.x, -from.y)
-      .scale(sx, sy)
-      .compose(this)
-      .translate(+from.x, +from.y);
-
-    this.invTransform.copy(this.transform).invert();
+  fit() {
+    if (this.selection.length === 1) {
+      this.obb.copy(this.selection[0].obb);
+      return;
+    }
 
     this.obb.min.put(+Infinity);
     this.obb.max.put(-Infinity);
 
-    // apply to each item
     for (const renderable of this.selection) {
-      const snapshot = this.snapshots.get(renderable)!;
+      this.#selectionOBB.copy(renderable.obb).transform(this.#invTransform);
+      this.obb.merge(this.#selectionOBB);
+    }
+
+    this.obb.transform(this.#transform);
+  }
+
+  apply(pivot = this.obb.center) {
+    this.#pivot.copy(pivot).transform(this.#invTransform);
+
+    const sx = this.#width !== 0 ? this.width / this.#width : 1;
+    const sy = this.#height !== 0 ? this.height / this.#height : 1;
+
+    this.#transform
+      .identity()
+      .translate(-this.#pivot.x, -this.#pivot.y)
+      .scale(sx, sy)
+      .compose(this)
+      .translate(+this.#pivot.x, +this.#pivot.y);
+
+    this.#invTransform.copy(this.#transform).invert();
+
+    for (const renderable of this.selection) {
+      const snapshot = this.#snapshots.get(renderable)!;
 
       renderable.width = snapshot.width;
       renderable.height = snapshot.height;
 
-      this.#transform
-        .copy(snapshot.screenTransform)
-        .transform(this.transform)
-        .transform(snapshot.invParentTransform);
+      this.#selectionTransform
+        .copy(snapshot.transform)
+        .transform(this.#transform)
+        .transform(snapshot.invParentTransform)
+        .decompose(renderable);
 
-      this.#transform.decompose(renderable);
-
-      renderable.update(false, true, true);
-
-      this.#obb.copy(renderable.obb).transform(this.invTransform);
-      this.obb.merge(this.#obb);
+      renderable.update();
     }
 
-    this.width = this.obb.width * sx;
-    this.height = this.obb.height * sy;
-
-    this.obb.transform(this.transform);
+    this.fit();
   }
 }
