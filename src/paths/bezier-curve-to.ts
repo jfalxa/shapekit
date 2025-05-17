8bafa31e6f19c6782f41104ea8cb57e2b7c0a138
jfalxa <jfalxa.sta@gmail.com>
2025-05-17 20:04:17 +0200

    add smooth curves

--- src/paths/bezier-curve-to.ts
+++ src/paths/bezier-curve-to.ts
@@ -1,3 +1,4 @@
+import { QuadraticCurveTo } from "./quadratic-curve-to";
 import { Segment, trackSegment } from "./segment";
 
 export function bezierCurveTo(
@@ -5,22 +6,58 @@ export function bezierCurveTo(
   cp1y: number,
   cp2x: number,
   cp2y: number,
-  x: number,
-  y: number
+  x?: number,
+  y?: number
 ) {
   return new BezierCurveTo(cp1x, cp1y, cp2x, cp2y, x, y);
 }
 
 export class BezierCurveTo extends Segment {
+  cp1x?: number;
+  cp1y?: number;
+  _cp1x!: number;
+  _cp1y!: number;
+
+  cp2x: number;
+  cp2y: number;
+
   constructor(
-    public cp1x: number,
-    public cp1y: number,
-    public cp2x: number,
-    public cp2y: number,
-    x: number,
-    y: number
+    cp1x: number,
+    cp1y: number,
+    cp2x: number,
+    cp2y: number,
+    x?: number,
+    y?: number
   ) {
-    super(x, y);
+    super(x ?? cp2x, y ?? cp2y);
+
+    if (x !== undefined && y !== undefined) {
+      this.cp1x = cp1x;
+      this.cp1y = cp1y;
+      this.cp2x = cp2x;
+      this.cp2y = cp2y;
+    } else {
+      this.cp2x = cp1x;
+      this.cp2y = cp1y;
+    }
+  }
+
+  link(previous: Segment | undefined): void {
+    if (this.cp1x !== undefined && this.cp1y !== undefined) {
+      this._cp1x = this.cp1x;
+      this._cp1y = this.cp1y;
+    } else if (previous instanceof QuadraticCurveTo) {
+      this._cp1x = previous.x * 2 - previous._cpx;
+      this._cp1y = previous.y * 2 - previous._cpy;
+    } else if (previous instanceof BezierCurveTo) {
+      this._cp1x = previous.x * 2 - previous.cp2x;
+      this._cp1y = previous.y * 2 - previous.cp2y;
+    } else if (previous) {
+      this._cp1x = previous.x;
+      this._cp1y = previous.y;
+    } else {
+      throw new Error("Missing control point");
+    }
   }
 }
 
--- src/paths/quadratic-curve-to.ts
+++ src/paths/quadratic-curve-to.ts
@@ -1,17 +1,47 @@
+import { BezierCurveTo } from "./bezier-curve-to";
 import { Segment, trackSegment } from "./segment";
 
 export function quadraticCurveTo(
   cpx: number,
   cpy: number,
-  x: number,
-  y: number
+  x?: number,
+  y?: number
 ) {
   return new QuadraticCurveTo(cpx, cpy, x, y);
 }
 
 export class QuadraticCurveTo extends Segment {
-  constructor(public cpx: number, public cpy: number, x: number, y: number) {
-    super(x, y);
+  cpx?: number;
+  cpy?: number;
+  _cpx!: number;
+  _cpy!: number;
+
+  constructor(cpx: number, cpy: number, x?: number, y?: number) {
+    super(x ?? cpx, y ?? cpy);
+
+    if (x !== undefined && y !== undefined) {
+      this.cpx = cpx;
+      this.cpy = cpy;
+    }
+  }
+
+  link(previous: Segment | undefined) {
+    super.link(previous);
+    if (this.cpx !== undefined && this.cpy !== undefined) {
+      this._cpx = this.cpx;
+      this._cpy = this.cpy;
+    } else if (previous instanceof QuadraticCurveTo) {
+      this._cpx = previous.x * 2 - previous._cpx;
+      this._cpy = previous.y * 2 - previous._cpy;
+    } else if (previous instanceof BezierCurveTo) {
+      this._cpx = previous.x * 2 - previous.cp2x;
+      this._cpy = previous.y * 2 - previous.cp2y;
+    } else if (previous) {
+      this._cpx = previous.x;
+      this._cpy = previous.y;
+    } else {
+      throw new Error("Missing control point");
+    }
   }
 }
 
